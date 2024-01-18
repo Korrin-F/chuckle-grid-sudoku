b9b8cd752b225bd6d13c758473226232c334df66
Korrin-F <96596840+Korrin-F@users.noreply.github.com>
2024-01-18 20:42:51 +0000

    created footer and add flex-grow to game page section

--- src/components/GameGrid/GridSection.jsx
+++ src/components/GameGrid/GridSection.jsx
@@ -3,11 +3,11 @@ import Grid from "./Grid"
 function GridSection() {
     return (
         <section
-            className='container-fluid'
+            className='container-fluid d-flex flex-grow-1'
             id='grid-section'
         >
             <div 
-                className='row justify-content-center text-center my-3'
+                className='row justify-content-center text-center my-3 m-auto'
                 id='grid-row'
             >
                 <Grid />
