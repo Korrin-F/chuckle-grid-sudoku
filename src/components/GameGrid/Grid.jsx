5f03d164e09be6bb12df8ec0ce0290a611de1d57
M4r1aN24 <142494616+M4r1aN24@users.noreply.github.com>
2024-01-19 18:20:55 +0000

    changes on the game page - updated grid module with api call

--- src/components/GameGrid/Grid.jsx
+++ src/components/GameGrid/Grid.jsx
@@ -1,4 +1,6 @@
 import Box from "./Box";
+import axios from "axios";
+import { useState, useEffect } from 'react';
 
 const styles = {
   width: "500px",
@@ -9,6 +11,48 @@ const styles = {
 };
 
 const boxes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+
+
+
+
+function GameApi() {
+
+    const [unsolvedDataSudoku, setUnsolvedDataSudoku] = useState([])
+    console.log(unsolvedDataSudoku);
+    
+      useEffect(() => {
+        async function sudokuApi(){
+
+            const difficulty = '';
+            const options = {
+            method: 'GET',
+            url: 'https://sudoku-board.p.rapidapi.com/new-board',
+            params: {
+              diff: difficulty,
+              stype: 'list',
+              solu: 'true'
+            },
+            headers: {
+              'X-RapidAPI-Key': '2e13952d57msh0b9a07691f3383ep15d154jsn3c910cbadbe9',
+              'X-RapidAPI-Host': 'sudoku-board.p.rapidapi.com'
+            }
+          };
+          
+          try {
+              const response = await axios.request(options);
+            //   console.log(response.data);
+        
+            const unsolvedDataSudoku = response.data.response['unsolved-sudoku'];
+        
+            setUnsolvedDataSudoku(unsolvedDataSudoku);
+        
+          } catch (error) {
+              console.error(error);
+          }}
+          sudokuApi();
+      }, [])
+
+}
 
 function Grid({ unsolvedDataSudoku: arr }) {
   if (arr.length === 0) {
--- src/pages/Game/Game.jsx
+++ src/pages/Game/Game.jsx
@@ -1,48 +1,15 @@
+import Container from 'react-bootstrap/Container';
 import GridSection from "../../components/GameGrid/GridSection";
-import axios from "axios";
-import { useState, useEffect } from 'react';
+import UnderGameBoard from "../../components/GameSections/UnderGameBoard";
+import AboveGameBoard from '../../components/GameSections/AboveGameBoard';
 
 function Game() {
-
-    const [unsolvedDataSudoku, setUnsolvedDataSudoku] = useState([])
-    console.log(unsolvedDataSudoku);
-    
-      useEffect(() => {
-        async function sudokuApi(){
-
-            const difficulty = '';
-            const options = {
-            method: 'GET',
-            url: 'https://sudoku-board.p.rapidapi.com/new-board',
-            params: {
-              diff: difficulty,
-              stype: 'list',
-              solu: 'true'
-            },
-            headers: {
-              'X-RapidAPI-Key': '2e13952d57msh0b9a07691f3383ep15d154jsn3c910cbadbe9',
-              'X-RapidAPI-Host': 'sudoku-board.p.rapidapi.com'
-            }
-          };
-          
-          try {
-              const response = await axios.request(options);
-            //   console.log(response.data);
-        
-            const unsolvedDataSudoku = response.data.response['unsolved-sudoku'];
-        
-            setUnsolvedDataSudoku(unsolvedDataSudoku);
-        
-          } catch (error) {
-              console.error(error);
-          }}
-          sudokuApi();
-      }, [])
-    return (
-        <>
-            <GridSection unsolvedDataSudoku = {unsolvedDataSudoku}/>
-        </>
+  return (
+    <Container  className="mt-2 flex-grow-1">
+        <AboveGameBoard />
+        <GridSection />
+        <UnderGameBoard />
+    </Container>
     )
 }
-
 export default Game;
