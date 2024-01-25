8854d9fc00c1064aa1078d35cc79d98203311293
Korrin-F <96596840+Korrin-F@users.noreply.github.com>
2024-01-25 01:10:15 +0000

    score width responsive

--- src/components/GameSections/AboveGameBoard.jsx
+++ src/components/GameSections/AboveGameBoard.jsx
@@ -4,22 +4,35 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Score from "./Score";
 import EndGameButton from './EndGameButton';
+import Stack from 'react-bootstrap/Stack';
 
 
 function AboveGameBoard(props) {
     const { score, sudokuBoard, updateSudokuBoard, solution } = props;
   return (
-        <Container className="mt-4">
-            <Row className="justify-content-end">
-                <Col className='d-flex flex-col justify-content-end col-12'>
+        // <Container className="mt-4">
+            <Row className="mt-4 justify-content-center w-100">
+                <Col xs={10} md={9} lg={8} xl={7} xxl={6}>
+                    <Stack gap={2} >
+                        <Row className="justify-content-end">
+                            <Score score={score} />
+                        </Row>
+                        <Row className="justify-content-end">
+                            <SolutionButton sudokuBoard={sudokuBoard} updateSudokuBoard={updateSudokuBoard} solution={solution}/>
+                            <EndGameButton score={score}/>
+                        </Row>
+                    </Stack>
+                    
+                </Col>
+                {/* <Col className='d-flex flex-col justify-content-end col-12'>
                     <Score score={score} />
                 </Col>
                 <Col className='d-flex flex-col justify-content-end col-12'>
                     <SolutionButton sudokuBoard={sudokuBoard} updateSudokuBoard={updateSudokuBoard} solution={solution}/>
                     <EndGameButton score={score}/>
-                </Col>
+                </Col> */}
             </Row>
-        </Container>
+        // </Container>
   );
 }
 
