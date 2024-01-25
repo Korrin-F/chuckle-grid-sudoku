74706c61783a35e00bb80114f60a42d2a50f5b61
Korrin-F <96596840+Korrin-F@users.noreply.github.com>
2024-01-25 16:46:41 +0000

    gamble button reveals tiles with different colored background now

--- src/components/Gamble/GambleButton.jsx
+++ src/components/Gamble/GambleButton.jsx
@@ -27,7 +27,10 @@ function GambleButton(props) {
     updateSudokuBoard,
     sudokuBoard,
     solution,
+    boardColors,
+    updateBoardColors,
   } = props;
+
   const [gambleResult, setGambleResult] = useState({
     text: "",
     buttonText: "",
@@ -47,6 +50,7 @@ function GambleButton(props) {
 
   const handleGamble = async () => {
     const random = Math.floor(Math.random() * 3);
+    // const random = 2; // for testing
     const randomAmount = Math.floor(Math.random() * 5) + 5;
 
     setAmountToReveal(randomAmount);
@@ -81,8 +85,12 @@ function GambleButton(props) {
     }
   };
 
+  const colors = ["var(--blue)", "var(--green)", "var(--pink)"];
+  let colorIndex = 0;
+
   const revealTiles = (amount) => {
     const newSudokuBoard = [...sudokuBoard];
+    const newBoardColors = [...boardColors];
 
     for (let i = 0; i < amount; i++) {
       let randomX = Math.floor(Math.random() * 9);
@@ -91,9 +99,13 @@ function GambleButton(props) {
       console.log("X:", randomX, "Y:", randomY, "Changed to:", solution[randomX][randomY]);
 
       newSudokuBoard[randomX][randomY] = solution[randomX][randomY];
+      newBoardColors[randomX][randomY] = colors[colorIndex];
+      // Cycle through colors
+      colorIndex = (colorIndex + 1) % colors.length;
     }
 
     updateSudokuBoard(newSudokuBoard);
+    updateBoardColors(newBoardColors);
   };
 
   const playAudio = (soundType) => {
@@ -128,55 +140,3 @@ function GambleButton(props) {
 export default GambleButton;
 
 
-// CODE FOR LATER
-//   const masterArray = [
-//     ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"],
-//     ["A4", "A5", "A6", "B4", "B5", "B6", "C4", "C5", "C6"],
-//     ["A7", "A8", "A9", "B7", "B8", "B9", "C7", "C8", "C9"],
-//     ["D1", "D2", "D3", "E1", "E2", "E3", "F1", "F2", "F3"],
-//     ["D4", "D5", "D6", "E4", "E5", "E6", "F4", "F5", "F6"],
-//     ["D7", "D8", "D9", "E7", "E8", "E9", "F7", "F8", "F9"],
-//     ["G1", "G2", "G3", "H1", "H2", "H3", "I1", "I2", "I3"],
-//     ["G4", "G5", "G6", "H4", "H5", "H6", "I4", "I5", "I6"],
-//     ["G7", "G8", "G9", "H7", "H8", "H9", "I7", "I8", "I9"]
-//  ]
-//  const starterArray = []
-
-//  useEffect(() => {
-//     for (let row of masterArray) {
-//         let miniArray = [];
-//       for (let cell of row) {
-//         let element = document.getElementById(cell);
-//         miniArray.push(element.value);
-//       }
-//         starterArray.push(miniArray);
-//     }
-
-//     console.log("starter array:", starterArray);
-//   }, []);
-
-    // const revealTiles = (amount) => {
-    //   const tilesToReveal = [];
-
-    //   for(let i = 0; i < amount; i++) {
-    //     const random = Math.floor(Math.random() * 82);
-    //     const randomCell = cellIdsFlat[random];
-    //     console.log(randomCell);
-    //     const cell = document.getElementById(randomCell);
-    //     const solution = cell.getAttribute("data-sol");
-    //     // make sure the cell is elegable to be revealed
-    //     // and make sure the same tile has not already be chosen
-    //     if (solution !== "0" && !tilesToReveal.includes(randomCell)) {
-    //       tilesToReveal.push(randomCell);
-    //     } else {
-    //       i--;
-    //     }
-    //   }
-
-    //   for (let tile of tilesToReveal) {
-    //     const cell = document.getElementById(tile);
-    //     const solution = cell.getAttribute("data-sol");
-    //     cell.value = solution;
-    //   }
-
-    // }
--- src/components/GameSections/UnderGameBoard.jsx
+++ src/components/GameSections/UnderGameBoard.jsx
@@ -5,13 +5,13 @@ import Col from 'react-bootstrap/Col';
 import MotivationButton from "../../components/Giphy/GameMotivation"
 
 function UnderGameBoard(props) {
-    const {updateScore, score, updateSudokuBoard, sudokuBoard, solution } = props;
+    const {updateScore, score, updateSudokuBoard, sudokuBoard, solution, boardColors, updateBoardColors } = props;
   return (
         // <Container className="mt-4 mb-5">
             <Row className="w-100 justify-content-center m-auto mb-5">
                 <Col className='d-flex flex-col justify-content-between col-12 col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4 p-0'>
                     {/* <Row className='justify-content-center'> */}
-                        <GambleButton score={score} updateScore={updateScore} updateSudokuBoard={updateSudokuBoard} sudokuBoard={sudokuBoard} solution={solution}/>
+                        <GambleButton score={score} updateScore={updateScore} updateSudokuBoard={updateSudokuBoard} sudokuBoard={sudokuBoard} solution={solution} boardColors={boardColors} updateBoardColors={updateBoardColors}/>
                         <MotivationButton />
                     {/* </Row> */}
                 </Col>
